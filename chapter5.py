2d476fb76aba1205f15484416195c46d56c51968
breyshaw <dawsonbrey@gmail.com>
2021-12-16 20:31:48 -0500

    add notes to ch5

--- chapter5.py
+++ chapter5.py
@@ -1,22 +1,28 @@
 import random
 import time
 
+# Defining functions using the def statement
 def displayIntro():
+  # Using triple quotes here allows to multiline strings.. easier to read.
   print('''You are in a land full of dragons. In front of you, 
   you see two caves. In one cave, the dragon is friendly
   and will share his treasure with you. The other dragon is 
   greedy and hungry, and will eat you on sight''')
   print()
 
+# while loop will run as long as a condition is true
 def chooseCave():
+  # cave is a locally scoped variable
   cave = ''
   while cave != '1' and cave != '2':
     print('Which cave will you go into?(1 or 2)')
+    # assigning the users input to the cave variable
     cave = input()
 
     return cave
 
 def checkCave(chosenCave):
+  #chosenCave is the parameter here in the definition. During the execution of the function the chosenCave from the chooseCave() function is being passed here.
   print('You approach the cave....')
   time.sleep(2)
   print('It is dark and spooky.....')
@@ -32,10 +38,12 @@ def checkCave(chosenCave):
   else:
       print('Gobbles you down in one bite!')
 
+# This is the games whole flow
 playAgain = 'yes'
 while playAgain == 'yes' or playAgain == 'y':
   displayIntro()
   caveNumber= chooseCave()
+  #caveNumber from above is passed to checkCave as an argument
   checkCave(caveNumber)
   print('Do you want to play again? (Yes or no)')
   playAgain = input()
